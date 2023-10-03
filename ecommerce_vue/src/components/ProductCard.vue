2a5728b91637062b40619c5a69e00bee01f4d6fc
StefaniaP93 <144233800+StefaniaP93@users.noreply.github.com>
2023-10-03 10:09:22 +0200

    Update ProductCard.vue

--- ecommerce_vue/src/components/ProductCard.vue
+++ ecommerce_vue/src/components/ProductCard.vue
@@ -4,7 +4,7 @@
             <img v-bind:src="product.get_thumbnail" class="card-img-top image" alt="...">
             <div class="card-body">
                 <h5 class="card-title">{{ product.name }}</h5>
-                <p class="card-text">{{ product.descrriptions }}</p>
+                <p class="card-text">{{ product.desctription }}</p>
                 <p class="card-text">{{ product.price }}</p>
                 <router-link v-bind:to="product.get_absolute_url" class="btn btn-secondary">View
                     details</router-link>
